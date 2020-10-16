230792b1ba191557a723c981ac3ec44e717dcb96
Eduardo Donato <eduardo.donato@academico.ifpb.edu.br>
2020-10-16 16:05:55 -0300

    first commit

---
+++ src/main/java/com/iCleanApi/ICleanApiApplication.java
@@ -0,0 +1,13 @@
+package com.iCleanApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICleanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICleanApiApplication.class, args);
+	}
+
+}
